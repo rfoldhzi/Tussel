ecd59ad11b04053933a5a50515173c0c7a5429c3
rfoldhzi <89109947+rfoldhzi@users.noreply.github.com>
2022-08-08 13:19:24 -0700

    Glob Faction: Units and Abilities
    Adding entries into UnitDB for all glob faction units and adding corresponding abilities to game.py

--- game.py
+++ game.py
@@ -662,10 +662,8 @@ class Game:
                             if getattr(par,'maxPopulation',False): #Reduces population of parent
                                 par.population = max(0,par.population-1)
             
-            if 'deathSpawn' in u.abilities:
-                newUnit = Unit(u.position,u.abilities['deathSpawn'],u.UnitID)
-                self.upgradeUnit(newUnit, i)
-                self.units[self.getPlayerfromUnit(u)].append(newUnit)
+            GoodToDeathSpawn = True #Small check used to make sure "deathspawn" is good to go (things preventing it include "convert")
+
             if u in hunterList: #For abilities that the hunters may have.
                 hunter = hunterList[u]
                 print('there is a hunter', hunter.name, hunter)
@@ -684,6 +682,19 @@ class Game:
                         print('It should work')
                         hunter.state = 'move'
                         hunter.stateData = u.position
+                #"convert" means the defeated unit joins the opposing team (potential for broken combos with cross faction units)
+                elif 'convert' in hunter.abilities:
+                    print("philsophy")
+                    newUnit = Unit(u.position,u.name,None, u.score)
+                    self.scores[hunterPlayer] += u.score
+                    self.units[hunterPlayer].append(newUnit)
+
+                    GoodToDeathSpawn = False
+            
+            if GoodToDeathSpawn and 'deathSpawn' in u.abilities:
+                newUnit = Unit(u.position,u.abilities['deathSpawn'],u.UnitID)
+                self.upgradeUnit(newUnit, i)
+                self.units[self.getPlayerfromUnit(u)].append(newUnit)
 
         for u in RemoveList:#more destroy
             print(u, u.name, 'is destroyed')
@@ -860,6 +871,18 @@ class Game:
                             newUnit = Unit(u.stateData[0],u.stateData[1],u.UnitID, score)
                             self.upgradeUnit(newUnit, i)
                             self.units[i].append(newUnit)
+
+                            #"default_transport" means this transporter unit starts with a list of units in its cargo
+                            if 'default_transport' in newUnit.abilities:
+                                #Kind of neglecting score here
+                                newUnit.carrying = []
+                                newUnit.population = len(newUnit.abilities['default_transport'])
+                                for transporteeName in newUnit.abilities['default_transport']:
+                                    #Same default position (though doesn't matter since transported)
+                                    transportee = Unit(u.stateData[0],transporteeName) 
+                                    self.upgradeUnit(transportee, i)
+                                    newUnit.carrying.append(transportee)
+                                
 
                             #"multibuild" tries to build multiple units at once
                             if 'multibuild' in u.abilities:
